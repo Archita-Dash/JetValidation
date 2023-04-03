9aa06f003bfac2244aaf118c10349dd00275ff8e
Archita <archita.rani.dash@cern.ch>
2023-04-03 11:14:20 +0200

    fixed track cuts

--- AliAnalysisTaskEmcalJetValidation.cxx
+++ AliAnalysisTaskEmcalJetValidation.cxx
@@ -246,7 +246,7 @@ void AliAnalysisTaskEmcalJetValidation::ExecOnceLocal(){
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     fTrackCuts = new AliESDtrackCuts("AliESDtrackCuts", "default");
     fTrackCuts->SetName("Global Hybrid tracks, loose DCA");
-    fTrackCuts->SetPtRange(0.1, 1.e10);
+    fTrackCuts->SetPtRange(0.15, 1.e15);
     fTrackCuts->SetEtaRange(-0.9, +0.9);
     fTrackCuts->SetRequireITSRefit(kTRUE);
     fTrackCuts->SetRequireTPCRefit(kTRUE);
@@ -259,14 +259,12 @@ void AliAnalysisTaskEmcalJetValidation::ExecOnceLocal(){
     fTrackCuts->SetClusterRequirementITS(AliESDtrackCuts::kSPD,
                                           AliESDtrackCuts::kAny);    // similar to SetRequireHitsInITSLayers(1, {0, 1}) in O2
     fTrackCuts->SetMaxChi2PerClusterITS(36.0);
-    fTrackCuts->SetMaxDCAToVertexXYPtDep("(0.0105 + 0.0350 / TMath::Power(pt, 1.1))"); //similar to "SetMaxDcaXYPtDep" implemented in O2 task (?)
-
     fTrackCuts->SetMaxChi2TPCConstrainedGlobal(36);              // same as SetRequireGoldenChi2(true) in O2
     fTrackCuts->SetMaxFractionSharedTPCClusters(0.4);           //Not yet in O2
 
     fTrackCuts->SetAcceptKinkDaughters(kFALSE);                 //Not yet in O2 task
     fTrackCuts->SetMaxDCAToVertexXY(2.4);
-    fTrackCuts->SetMaxDCAToVertexZ(2.0);                        // similar to SetMaxDcaZ in O2 task
+    fTrackCuts->SetMaxDCAToVertexZ(3.2);                        // similar to SetMaxDcaZ in O2 task
     //fTrackCuts->SetDCAToVertex2D(kTRUE);
     fTrackCuts->SetDCAToVertex2D(kFALSE);                          //Marta suggested to set the flag as kFalse
                                                                   //because there's no option to use the 2D cut for the DCA in O2
